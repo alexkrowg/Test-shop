eca17e3d4e08f2f6b48ed3d71e49515e89701035
Igor Nichiforov <70261890+Igor-Nix@users.noreply.github.com>
2021-10-20 11:15:05 +0200

    Homegrid done

--- src/components/homegrid/Homegrid.js
+++ src/components/homegrid/Homegrid.js
@@ -1,27 +1,8 @@
 import React from "react";
 import "./homegrid.css";
+import { data } from "../../data";
 
 function Homegrid() {
-  const data = [
-    {
-      id: 1,
-      img: "https://cdn.shopify.com/s/files/1/0107/9820/2938/products/gmmobile1_grande.png?v=1629754332",
-      title: "shoes",
-      price: "10$",
-    },
-    {
-      id: 2,
-      img: "https://img.joomcdn.net/a24f70d27ca2552e92aa45c9005827c82b915156_original.jpeg",
-      title: "shirt",
-      price: "200$",
-    },
-    {
-      id: 3,
-      img: "https://productimages.hepsiburada.net/l/32/600-800/10359186194482.jpg",
-      title: "hoodie",
-      price: "100$",
-    },
-  ];
   return (
     <>
       <div className="featured-items">
